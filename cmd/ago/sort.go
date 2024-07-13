b6ed8f6e71f21673fdabc30f50ffde6edeb0472b
Paul Crane <paul@acheronsoft.com>
2024-07-13 16:23:43 -0500

    Add sorting functions to sort.go

---
+++ cmd/ago/sort.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"sort"
+)
+
+func sortByModTime(files []os.DirEntry) []os.DirEntry {
+	sort.Slice(files, func(i, j int) bool {
+		infoI, _ := files[i].Info()
+		infoJ, _ := files[i].Info()
+		return infoI.ModTime().Unix() > infoJ.ModTime().Unix()
+	})
+
+	return files
+}
+
+func sortBySize(files []os.DirEntry) []os.DirEntry {
+	sort.Slice(files, func(i, j int) bool {
+		infoI, _ := files[i].Info()
+		infoJ, _ := files[i].Info()
+		return infoI.Size() > infoJ.Size()
+	})
+
+	return files
+}
+
+func sortByFileName(files []os.DirEntry) []os.DirEntry {
+	sort.Slice(files, func(i, j int) bool {
+		infoI, _ := files[i].Info()
+		infoJ, _ := files[i].Info()
+		return infoI.Name() > infoJ.Name()
+	})
+
+	return files
+}
