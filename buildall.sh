2d85eb636ddaa957d592f8a5271dc963ba18fbd7
Paul Crane <paul@acheronsoft.com>
2025-10-24 09:57:47 -0500

    fix: error occured due to stray character in script

--- buildall.sh
+++ buildall.sh
@@ -2,20 +2,19 @@
 
 platforms=("windows/amd64" "linux/amd64")
 
-for platform in "${platforms[@]}"
-do
-    platform_split=(${platform//\// })
-    GOOS=${platform_split[0]}
-    GOARCH=${platform_split[1]}
+for platform in "${platforms[@]}"; do
+	platform_split=(${platform//\// })
+	export GOOS=${platform_split[0]}
+	export GOARCH=${platform_split[1]}
 
-    if [ $GOOS = "windows" ]; then
-        output_name+='.exe'
-    fi
+	if [ $GOOS = "windows" ]; then
+		output_name+='.exe'
+	fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build
-m
-    if [ $? -ne 0 ]; then
-        echo 'An error has occurred! Aborting the script execution...'
-        exit 1
-    fi
+	GOOS=$GOOS GOARCH=$GOARCH go build -C ./cmd/ago/
+
+	if [ $? -ne 0 ]; then
+		echo 'An error has occurred! Aborting the script execution...'
+		exit 1
+	fi
 done
